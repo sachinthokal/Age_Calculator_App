9435a8ca47852d026d5c0a359936b6a102a34082
sachinthokal <sachinthokal1799@gmail.com>
2023-06-24 23:22:51 +0530

    Header & Footer Completed

---
+++ src/app/age-calculator/age-calculator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-age-calculator',
+  templateUrl: './age-calculator.component.html',
+  styleUrls: ['./age-calculator.component.css']
+})
+export class AgeCalculatorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AgeCalculatorComponent } from './age-calculator/age-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AgeCalculatorComponent
   ],
   imports: [
     BrowserModule,
